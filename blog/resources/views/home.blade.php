6a2c9c9751bb6425ed60531b3accaf0297293ced
Sam Ourn <sourn@mail.greenriver.edu>
2019-05-02 13:34:52 -0400

    Added some options

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <h1 class="display-4">Lets Get This Bread!</h1>
             <p class="lead">Bread is delicious! So delicious, I want to be bread!</p>
             <hr class="my-4">
-            <p>So what type a bread is ya, anyways?</p>
+            <p>So what type a bread is ya, anyway?</p>
             <p class="lead">
                 <a class="btn btn-secondary btn-xl" href="q1" role="button">Take the Quiz And Find Out!</a>
             </p>
